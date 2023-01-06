e71827ab65d706df3f98a07bb197e0a45e8e4f26
Marcantoine4581 <marcantoine.vannier@gmail.com>
2023-01-06 01:22:15 -0800

    sort.h updated

--- sort.h
+++ sort.h
@@ -25,4 +25,6 @@ void quick_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+
 #endif
